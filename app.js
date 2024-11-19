a7b677a2c74f5405546e95fe99cc4cd7fff77143
buddy <65063402+titanbuddy@users.noreply.github.com>
2024-11-19 20:01:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const burger = document.querySelector('.burger');
+const navLink = document.querySelector('nav .links');
+const linksDropdown = document.querySelector('.links-dropdown');
+const societyLink = document.querySelector('.society-link a');
+societyLink.addEventListener('click', () => {
+    linksDropdown.classList.toggle('dropdown-appear')
+});
+burger.addEventListener('click', ()=> {
+    navLink.classList.toggle('nav-active')
+});
